36704e24cc3322af8e5c0f17464d3912706defda
Scott Morey <smorey@tadl.org>
2022-07-13 18:18:20 -0400

    hello world

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+    def create
+      user = User.from_omniauth(request.env['omniauth.auth'])
+      if user != false
+        session[:user_id] = user.id
+        url = request.env['omniauth.origin']
+        redirect_to url
+      else
+        redirect_to root_url, :alert => Settings.error_msg.invalid_domain
+      end
+    end
+  
+    def destroy
+      puts 'sam was here'
+      session.delete(:user_id)
+      redirect_to root_url
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'main/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "main#index"
+
+  # Log in, failed login, log Out 
+  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
+
+end
