fa9f407e39421dbe0f857b8a031a5fac8882855b
Scott Morey <smorey@tadl.org>
2022-09-01 16:15:03 +0000

    another try to fix import map bug

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "./popper";
-import "./bootstrap.min";
+import "popper"
+import "bootstrap"
 import 'custom/admin'
 import 'custom/incidents'
 import 'custom/patrons'
--- config/importmap.rb
+++ config/importmap.rb
@@ -3,5 +3,5 @@
 pin "application", preload: true
 pin "jquery", to: "https://code.jquery.com/jquery-3.6.0.min.js", preload: true
 pin_all_from "app/javascript/custom", under: "custom"
-pin "bootstrap", to: "bootstrap.min.js", preload: true
-pin "@popperjs/core", to: "popper.js", preload: true
+pin "popper", to: 'popper.js', preload: true
+pin "bootstrap", to: 'bootstrap.min.js', preload: true
