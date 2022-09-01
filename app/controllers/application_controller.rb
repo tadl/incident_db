3406f1189d0e88a1a5187a4a5d7765b01a33f6c4
Scott Morey <smorey@tadl.org>
2022-09-01 16:22:57 +0000

    skip authenticity check because we are doing so wild proxy stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
     config.time_zone = 'Eastern Time (US & Canada)'
+    skip_before_filter :verify_authenticity_token  
+    
     def current_user
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
