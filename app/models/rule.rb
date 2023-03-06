4616333a5a40da3f05c34194ab7d4627ef327abe
Scott Morey <smorey@tadl.org>
2023-03-06 19:40:23 +0000

    no need to validate unique description

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -2,8 +2,7 @@ class Rule < ApplicationRecord
     validates :track, presence: { message: 'Please select a track' }
     validates :track, acceptance: {accept: ['A', 'B', 'None'], message: "Invalid track"}
     validates :description, presence: { message: 'Description required' }, allow_blank: false
-    validates :description, uniqueness: { message: 'An infraction type with this description already exisits'}
-
+    
     def violation_format
         violation_format = self.track + self.description
     end
